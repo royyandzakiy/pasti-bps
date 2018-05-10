83903c5b5dbb67ffcf988f5682656eb43cf238f4
royyandzakiy <royyandzakiy@students.itb.ac.id>
2018-05-10 22:11:21 +0700

    fix: allow change page

--- change_page.php
+++ change_page.php
@@ -23,11 +23,13 @@
         $index_in_list = array_search($konseptopik_aktif, $list_konseptopik);    
         $index_next = $index_in_list < 15 ? $index_in_list + 1 : $index_in_list;
         $index_back = $index_in_list > 0 ? $index_in_list - 1 : $index_in_list;
+        $next = $list_konseptopik[$index_next];
+        $back = $list_konseptopik[$index_back];
     
     if ($_GET['goto'] == 'next') {
-        $goto = $list_konseptopik[$index_next];    
+        $goto = $next;    
     } else if ($_GET['goto'] == 'back') {
-        $goto = $list_konseptopik[$index_back];
+        $goto = $back;
     } else {
         $goto = $_GET['goto'];
     }
@@ -35,15 +37,18 @@
     // TENTUKAN LOGIC ALUR PERPINDAHAN DISINI
     $allowed = true; // bruteforce
 
-    if ($goto != '0101') {
+    if ($goto != '0101') { // 0101 pasti boleh
         if (($goto == '0201' && $_SESSION['level_pengetahuan'] < 16) || ($goto == '0301' && $_SESSION['level_pengetahuan'] < 50)) {
+            // IF TEST
             $allowed = false;
         } else {
-            $query = "SELECT * FROM riwayattopik WHERE id_siswa = $id_siswa AND id_topik = " . $list_konseptopik[array_search($goto, $list_konseptopik)-1];
+            // IF NOT TEST
+            // CHECK IF: TOPIK BEFORE EVER VISITED, IF YES ALLOW
+            $query = "SELECT * FROM riwayattopik WHERE id_siswa = $id_siswa AND id_topik = " . $back;
             $result = $con->query($query);
             $row = $result->fetch(PDO::FETCH_NUM);
             
-            $allowed = $row != NULL ? true : false;
+            $allowed = ($row != false ? true : false);
         }
     }
     echo "Allowed: " . $allowed . "<br />";
@@ -52,9 +57,25 @@
         // ubah session sebagai posisi terakhir
         $_SESSION['konsep_aktif'] = substr($goto,0,2);
         $_SESSION['topik_aktif'] = substr($goto,2,2);
+
+        // UPDATE KONSEP&TOPIK TERAKHIR
+        // check konsep&topik_terakhir
+        $query = "SELECT konsep_terakhir, topik_terakhir FROM users WHERE nip = $id_siswa";
+        $result = $con->query($query);
+
+        if ($row = $result->fetch(PDO::FETCH_NUM)) {
+            // update konsep&topik terakhir
+            $konsep_terakhir = (string) $row[0];
+            $topik_terakhir = (string) $row[1];
+            if (($konsep_terakhir . $topik_terakhir) < $goto) {
+                $query = "UPDATE users SET konsep_terakhir = '". substr($goto,0,2) ."', topik_terakhir = '". substr($goto,2,2) ."' WHERE nip = $id_siswa";
+            }
+            $result = $con->query($query);
+        }
     }
 
-    echo $goto;
+    echo "MENUJU TOPIK: " . $goto . "<br />";
+    echo "ke: " . $_SESSION['konsep_aktif'] . $_SESSION['topik_aktif'];
 
     // periksa jika tipe laman adalah tes
     $not_test = array_search($goto,$list_konseptopik_tes) === false;
